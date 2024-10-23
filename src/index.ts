5ac2b6e632586e1bf562da6ae853488c6b1fa848
= <=>
2024-10-23 17:01:24 -0300

    Se usa ruta diaries

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import express from 'express'
+import diaryroutes from './routes/diaries'
 
 const app = express()
 
 app.use(express.json())
 const PORT = 3000
 
+
+
 // Definir una ruta
 app.get('/', (_req, res) => {
     res.send('¡Hola, mundo!');
     console.log('App get')
   });
+
+app.use('/api/diaries', diaryroutes)
+
   
   // Iniciar el servidor
   app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
   });
+
