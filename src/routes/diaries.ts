c8b7801fc6110aa6c9e63bc42ba742bbbe5d1733
= <=>
2024-10-23 16:55:28 -0300

    Se configura ruta diaries

---
+++ src/routes/diaries.ts
@@ -0,0 +1,13 @@
+import Express from "express";
+
+const router = Express.Router()
+
+router.get('/', (req, res) => {
+    res.send('fetc diaries')
+})
+
+router.post('/', (req, res) =>{
+    res.send('Saving diaries')
+})
+
+export default router
